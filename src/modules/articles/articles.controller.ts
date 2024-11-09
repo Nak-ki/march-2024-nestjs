61f7a1ac62c369a982d598e589970b90012047e5
Nakk-i <nastyabond90@gmail.com>
2024-11-09 16:08:01 +0200

    Add tag

--- src/modules/articles/articles.controller.ts
+++ src/modules/articles/articles.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { ArticleID } from '../../common/types/entity-ids.type';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { IUserData } from '../auth/models/interfaces/user-data.interface';
 import { CreateArticleDto } from './models/dto/req/create-article.req.dto';
+import { ListArticleQueryDto } from './models/dto/req/list-article-query.dto';
 import { UpdateArticleDto } from './models/dto/req/update-article.req.dto';
 import { ArticleResDto } from './models/dto/res/article.res.dto';
+import { ArticleListResDto } from './models/dto/res/article-list.res.dto';
 import { ArticlesMapper } from './services/article.mapper';
 import { ArticlesService } from './services/articles.service';
 
@@ -14,22 +24,34 @@ import { ArticlesService } from './services/articles.service';
 @ApiTags('Articles')
 @Controller('articles')
 export class ArticlesController {
-  constructor(private readonly usersService: ArticlesService) {}
+  constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
   public async create(
     @CurrentUser() userData: IUserData,
     @Body() dto: CreateArticleDto,
   ): Promise<ArticleResDto> {
-    const result = await this.usersService.create(userData, dto);
+    const result = await this.articlesService.create(userData, dto);
     return ArticlesMapper.toResDto(result);
+  }
+
+  @Get()
+  public async findAll(
+    @CurrentUser() userData: IUserData,
+    @Query() query: ListArticleQueryDto,
+  ): Promise<ArticleListResDto> {
+    const [entities, total] = await this.articlesService.findAll(
+      userData,
+      query,
+    );
+    return ArticlesMapper.toResDtoList(entities, total, query);
   }
 
   @Get(':articleId')
   public async findOne(
     @Param('articleId') articleId: ArticleID,
   ): Promise<ArticleResDto> {
-    const result = await this.usersService.findOne(articleId);
+    const result = await this.articlesService.findOne(articleId);
     return ArticlesMapper.toResDto(result);
   }
 
@@ -39,7 +61,7 @@ export class ArticlesController {
     @Param('articleId') articleId: ArticleID,
     @Body() dto: UpdateArticleDto,
   ): Promise<ArticleResDto> {
-    const result = await this.usersService.update(userData, articleId, dto);
+    const result = await this.articlesService.update(userData, articleId, dto);
     return ArticlesMapper.toResDto(result);
   }
 }
